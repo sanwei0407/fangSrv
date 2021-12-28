43cab9eaf924f507a8fb70eb9b239b7e63cc8575
dixon <77533464@qq.com>
2021-12-28 17:19:25 +0800

    day2

--- app/controller/common.js
+++ app/controller/common.js
@@ -7,7 +7,8 @@ class commonController extends Controller {
   async bdSearch(){
     const { ctx } = this;
     const { keyword } = ctx.request.body;
-    const bmapak = '1RO8EvPHSDzApYU0pORqyXKEWAmGs3we';
+    const bmapak = '1RO8EvPHSDzApYU0pORqyXKEWAmGs3we'; // 服务端的key
+    // 使用到的接口文档地址 https://lbsyun.baidu.com/index.php?title=webapi/place-suggestion-api
     const res = await ctx.curl(`https://api.map.baidu.com/place/v2/suggestion?query=${keyword}&region=广州&city_limit=true&output=json&ak=${bmapak}`,{
       dataType: 'json',
     });
